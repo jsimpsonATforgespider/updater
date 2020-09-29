acaf576e8145f60ba516dba5db839e4ce28d121f
jakelsimpson <67084246+jakelsimpson@users.noreply.github.com>
2020-09-28 20:56:57 -0400

    Update mainPluginFile.php

--- mainPluginFile.php
+++ mainPluginFile.php
@@ -16,9 +16,9 @@
 
    function updaterCall(){
      $updater = new FS_Updater();
-     $updater->set_username('jsimpsonATforgespider') // add github username here
-     $updater->set_repository('updater') // add plugin repo here
-     $updater->authorize('6fffe3ea2180823a321b4177c0e906b31c9615fb') //entre api key here for plugin
+     $updater->set_username('jsimpsonATforgespider'); // add github username here
+     $updater->set_repository('updater'); // add plugin repo here
+     $updater->authorize('6fffe3ea2180823a321b4177c0e906b31c9615fb'); //entre api key here for plugin
      $updater->initialize();
    }
 
